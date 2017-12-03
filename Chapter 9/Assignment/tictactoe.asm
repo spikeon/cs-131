9f6b03ec198d9671701f520007b6a4b8b2ea34f8
Cory Upham <coryupham@Corys-MacBook-Pro.local>
2017-12-02 20:46:32 -0800

    adding comment

--- Chapter 9/Assignment/tictactoe.asm
+++ Chapter 9/Assignment/tictactoe.asm
@@ -15,17 +15,17 @@
 ;
 ; Changes from original:
 ;
-;  1.  Added INITBOARD subroutine to reinitialize square table and 
+;  1.  Added INITBOARD subroutine to reinitialize square table and
 ;      player so program can be run more than once by simply
 ;      resetting the PC to x3000.
 ;  2.  Included some addition helper code to simplify assignment.
 ;
 ;
-; ASSIGNMENT:  SEARCH FOR "***" AND ADD REQUIRED CODE TO 
+; ASSIGNMENT:  SEARCH FOR "***" AND ADD REQUIRED CODE TO
 ;              COMPLETE PROGRAM.
 ;
 ; NOTE:  After HALT (TRAP x25) is executed, if you run the program
-;        without reseting the PC to x3000, the memory will get 
+;        without reseting the PC to x3000, the memory will get
 ;        corrupted.  One symptom of corrupted memory is funny
 ;        characters on the displayed tic-tac-toe table. Just
 ;        reinitialize the machine (File->Reinitialize Machine)
@@ -55,16 +55,16 @@ MAIN		LEA 	R0, WELCOME
 CHECK		ADD	R1, R1, #0	; Load condition codes
 		BRp	GAMEOVER
 MOVE		LD	R2, GETVALIDINPUT_ADDR
-		JSRR	R2		
+		JSRR	R2
 ;-----------------------------------------------------------------
 ; After GETVALIDINPUT, R1 contains valid input char
 ;-----------------------------------------------------------------
-		LD	R0, NEG_ASCII_x	
-		ADD	R0, R0, R1	
+		LD	R0, NEG_ASCII_x
+		ADD	R0, R0, R1
 		BRz	FINALMESSAGE	; User opted to exit
 		;
-		LD	R0, NEG_ASCII_q	
-		ADD	R0, R0, R1	
+		LD	R0, NEG_ASCII_q
+		ADD	R0, R0, R1
 		BRz	CONTMESSAGE	; User opted to quit
 		;
 		LD	R2, UPDATEBOARD_ADDR
@@ -84,7 +84,7 @@ CONTMESSAGE	LEA	R0, PLAYAGAIN
 		BRnzp	MAIN
 FINALMESSAGE	LEA	R0, THANKS
 		TRAP	x22
-		TRAP	x25	
+		TRAP	x25
 ;-----------------------------------------------------------------
 ; Pointer table to subroutines.
 ;-----------------------------------------------------------------
@@ -110,19 +110,19 @@ ROW10		.STRINGZ	"   |   |   "
 ROW11 		.STRINGZ	"   |   |   "
 WELCOME		.STRINGZ	"\nWelcome to Tic-Tac-Toe"
 THANKS		.STRINGZ	"Thanks for playing Tic-Tac-Toe\n"
-ASCII_X		.FILL 		x0058	
-ASCII_O		.FILL		x004f	
+ASCII_X		.FILL 		x0058
+ASCII_O		.FILL		x004f
 NEG_ASCII_X	.FILL		xFFA8
 NEG_ASCII_O	.FILL		xFFB1
 NEG_ASCII_q	.FILL		xFF8F	; Negative of x0071 (ASCII 'q')
 NEG_ASCII_x	.FILL		x-78	; Negative of x0078 (ASCII 'x')
-NEG_ASCII_SP	.FILL		xFFE0   
+NEG_ASCII_SP	.FILL		xFFE0
 NEWLINE		.FILL		x000A	; LineFeed Character
 PLAYAGAIN	.STRINGZ	"Press any key to play another game of Tic-Tac-Toe\n"
 ;-----------------------------------------------------------------
 ; EVALBOARD subroutine -
-; Args: 
-;	R1 - Upon exit, contains a code indicating 
+; Args:
+;	R1 - Upon exit, contains a code indicating
 ;	     the status of the game.
 ;		0 = game still in progress
 ;		1 = X wins
@@ -136,7 +136,7 @@ PLAYAGAIN	.STRINGZ	"Press any key to play another game of Tic-Tac-Toe\n"
 EVALBOARD	ST	R0, EV_R0	; Save R0
 		ST	R2, EV_R2	; Save R2
 		ST	R3, EV_R3	; Save R3
-		ST	R4, EV_R4	; Save R4	
+		ST	R4, EV_R4	; Save R4
 		ST	R5, EV_R5	; Save R5
 		ST	R6, EV_R6	; Save R6
 		ST	R7, EV_R7	; Save Return Address
@@ -156,13 +156,13 @@ EVALBOARD	ST	R0, EV_R0	; Save R0
 		LD	R3, NEG_ASCII_SP
 		AND	R4, R4, #0	; R4 countains the value we'll store to EV_FULLBOARD
 EV_LOOP		ADD	R5, R0, R2
-		LDR	R5, R5, #0	; pointer to square n 
-		LDR	R5, R5, #0	; square n 
+		LDR	R5, R5, #0	; pointer to square n
+		LDR	R5, R5, #0	; square n
 		ADD	R5, R5, R3
 		BRz	EV_FOUNDSPACE	; if we find a space, note it and exit loop
-		ADD	R2, R2, #-1	
-		BRp	EV_LOOP		
-		BRnzp	EV_NOSPACE	
+		ADD	R2, R2, #-1
+		BRp	EV_LOOP
+		BRnzp	EV_NOSPACE
 EV_FOUNDSPACE	ADD	R4, R4, #1
 EV_NOSPACE	ST	R4, EV_FULLBOARD
 ;-----------------------------------------------------------------
@@ -174,107 +174,107 @@ EV_NOSPACE	ST	R4, EV_FULLBOARD
 		LDR	R2, R2, #0	; square 5 ('X', 'O', or ' ') = center square
 		LD	R3, NEG_ASCII_SP
 		ADD	R3, R3, R2
-		BRz	EV_CHECK5	; If the center square is a space, 
+		BRz	EV_CHECK5	; If the center square is a space,
 					; skip these four checks
-		NOT	R3, R2		
+		NOT	R3, R2
 		ADD	R3, R3, #1	; now we have ASCII_NEG_X or ASCII_NEG_O
 EV_CHECK1	ADD	R2, R0, #4	; pointer to pointer to square 4
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 4 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK2
 		ADD	R2, R0, #6	; pointer to pointer to square 6
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 6 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK2	ADD	R2, R0, #2	; pointer to pointer to square 2
-		LDR	R2, R2, #0	
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 2 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK3
 		ADD	R2, R0, #8	; pointer to pointer to square 8
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 8 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK3	ADD	R2, R0, #1	; pointer to pointer to square 1
-		LDR	R2, R2, #0	
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 1 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK4
 		ADD	R2, R0, #9	; pointer to pointer to square 9
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 9 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK4	ADD	R2, R0, #3	; pointer to pointer to square 3
-		LDR	R2, R2, #0	
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 3 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK5
 		ADD	R2, R0, #7	; pointer to pointer to square 7
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 7 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK5	ADD	R2, R0, #1	; pointer to pointer to square 1
-		LDR	R2, R2, #0		
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 1 ('X', 'O', or ' ') = top left
 		LD	R3, NEG_ASCII_SP
 		ADD	R3, R3, R2
-		BRz	EV_CHECK6	; If the top left square is a space, 
+		BRz	EV_CHECK6	; If the top left square is a space,
 					; skip these two checks
-		NOT	R3, R2		
+		NOT	R3, R2
 		ADD	R3, R3, #1	; now we have ASCII_NEG_X or ASCII_NEG_O
 		ADD	R2, R0, #4	; pointer to pointer to square 4
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 4 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK6
 		ADD	R2, R0, #7	; pointer to pointer to square 7
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 7 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK6	ADD	R2, R0, #2	; pointer to pointer to square 2
-		LDR	R2, R2, #0		
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 2 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK7
 		ADD	R2, R0, #3	; pointer to pointer to square 3
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 3 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK7	ADD	R2, R0, #9	; pointer to pointer to square 9
-		LDR	R2, R2, #0		
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 9 ('X', 'O', or ' ') = bottoom right
 		LD	R3, NEG_ASCII_SP
 		ADD	R3, R3, R2
-		BRz	EV_CHECK8	; If the bottom right square is a space, 
+		BRz	EV_CHECK8	; If the bottom right square is a space,
 					; skip these two checks
-		NOT	R3, R2		
+		NOT	R3, R2
 		ADD	R3, R3, #1	; now we have ASCII_NEG_X or ASCII_NEG_O
 		ADD	R2, R0, #6	; pointer to pointer to square 6
-		LDR	R2, R2, #0		
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 6 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECK8
 		ADD	R2, R0, #3	; pointer to pointer to square 3
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 3 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 EV_CHECK8	ADD	R2, R0, #8	; pointer to pointer to square 8
-		LDR	R2, R2, #0		
+		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 8 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRnp	EV_CHECKCAT
 		ADD	R2, R0, #7	; pointer to pointer to square 7
 		LDR	R2, R2, #0
 		LDR	R2, R2, #0	; square 7 ('X', 'O', or ' ')
-		ADD	R2, R2, R3	
+		ADD	R2, R2, R3
 		BRz	EV_WINNER	; found a winner!
 ;-----------------------------------------------------------------
 ; If we didn't have a winner, check to see if we had a cat game
@@ -284,14 +284,14 @@ EV_CHECKCAT	LD	R2, EV_FULLBOARD
 		ADD	R1, R1, #3	; R1 = 3 indicating we had a cat game.
 		BRnzp	EV_EXIT
 ;-----------------------------------------------------------------
-; If we have a winner, R3 contains the negative ascii code 
+; If we have a winner, R3 contains the negative ascii code
 ; of the winner's character
 ;-----------------------------------------------------------------
 EV_WINNER	LD	R2, ASCII_X
 		ADD	R2, R2, R3
-		BRnp	EV_O_WINS	
+		BRnp	EV_O_WINS
 		ADD	R1, R1, #1	; R1 = 1 indicating X won
-		BRnzp	EV_EXIT				
+		BRnzp	EV_EXIT
 EV_O_WINS	ADD	R1, R1, #2	; R1 = 2 indicating O won
 ;-----------------------------------------------------------------
 ; Restore the registers and return with exit code in R1
@@ -304,7 +304,7 @@ EV_EXIT		LD	R0, EV_R0	; Restore R0
 		LD	R6, EV_R6	; Restore R6
 		LD	R7, EV_R7	; Restore R7
 		RET
-EV_FULLBOARD	.BLKW 	1		
+EV_FULLBOARD	.BLKW 	1
 EV_R0		.BLKW	1
 EV_R2 		.BLKW	1
 EV_R3 		.BLKW	1
@@ -359,7 +359,7 @@ INITBOARD	LD	R0, SQRCENTERS_ADDR
 ;-----------------------------------------------------------------
 		AND	R2, R2, #0
 		ADD	R2, R2, #1
-		ST	R2, PLAYER		
+		ST	R2, PLAYER
 		RET
 IB_ASCII_SP	.FILL	x20		; ASCII space
 ;-----------------------------------------------------------------
@@ -378,7 +378,7 @@ GETPLAYER	LD 	R0, PLAYER	; Load R0 with player: 0=O, 1=X
 		LD 	R0, ASCII__X
 		BR 	GP_DONE
 GP_O_TURN	LD 	R0, ASCII__O
-GP_DONE		RET		
+GP_DONE		RET
 ;-----------------------------------------------------------------
 ; Pointer table to constants defined above.
 ;-----------------------------------------------------------------
@@ -386,10 +386,10 @@ GP_DONE		RET
 ; corresponds to one of the nine centers of a square
 SQRCENTERS	.BLKW	9
 SQRCENTERS_ADDR	.FILL 	SQRCENTERS
-ROW1_ADDR	.FILL	ROW1		
+ROW1_ADDR	.FILL	ROW1
 ROW2_ADDR	.FILL	ROW2
 ROW3_ADDR	.FILL	ROW3
-ROW4_ADDR	.FILL	ROW4	
+ROW4_ADDR	.FILL	ROW4
 ROW5_ADDR	.FILL	ROW5
 ROW6_ADDR	.FILL	ROW6
 ROW7_ADDR	.FILL	ROW7
@@ -399,13 +399,13 @@ ROW10_ADDR	.FILL	ROW10
 ROW11_ADDR	.FILL	ROW11
 GETPLAYER_ADDR	.FILL	GETPLAYER
 ASCII__X	.FILL	x0058 ; Notice there are two underscores
-ASCII__O	.FILL	x004F 
-NEW_LINE	.FILL	x000A 
+ASCII__O	.FILL	x004F
+NEW_LINE	.FILL	x000A
 ;-----------------------------------------------------------------
 ; UPDATEBOARD subroutine
 ; Args:
-;	R1 - contains an ASCII code for a number 1-9 
-; 
+;	R1 - contains an ASCII code for a number 1-9
+;
 ; 1. Based on MEMORY[PLAYER] you know whether X or O just took a turn.
 ; 2. Store the appropriate charater (X or O) in memory to the middle
 ;    of the Tic-Tac-Toe square denoted by the contents of R1.
@@ -420,7 +420,7 @@ UPDATEBOARD	ST 	R0, GI_R0	; save registers
 
 		LD 	R2, GETPLAYER_ADDR
 		JSRR	R2		; returns 'X' or 'O' in R0
-		
+
 		LD	R3, SQRCENTERS_ADDR
 		ADD	R3, R3, #-1	; R0 contains base of pointer table
 					; (handle indexes 1 - 9)
@@ -442,20 +442,20 @@ UB_DONE		ST	R2, PLAYER
 		LD 	R2, GI_R2
 		LD 	R3, GI_R3
 		LD 	R4, GI_R4
-		LD 	R7, GI_R7		
+		LD 	R7, GI_R7
 		RET
 ;-----------------------------------------------------------------
 ;++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-; DO NOT MODIFY ANY CODE ABOVE THIS LINE! 
+; DO NOT MODIFY ANY CODE ABOVE THIS LINE!
 ; MAKE ALL YOUR CHANGES AND ADDITIONS BELOW THIS LINE!
 ;-----------------------------------------------------------------
-; GETVALIDINPUT subroutine 
+; GETVALIDINPUT subroutine
 ; Args:
-;	R1 - When subroutine returns, this register 
+;	R1 - When subroutine returns, this register
 ;	     contains the input, ASCII 1-9, or ASCII 'q'.
-; 
+;
 ; 1. Prompt the current player for which square he/she wants.
-; 2. If the player inputs anything other than a number 1-9 or the 
+; 2. If the player inputs anything other than a number 1-9 or the
 ;    letter 'q', print an error message and reprompt them.
 ; 3. If the player chooses an occupied square, print an error message
 ;    and reprompt them.
@@ -473,7 +473,7 @@ GETVALIDINPUT	ST 	R0, GI_R0	; save registers
 ;-----------------------------------------------------------------
 GI_LOOP		LD 	R2, GETPLAYER_ADDR
 		JSRR	R2		; returns 'X' or 'O' in R0
-;                     
+;
 ; ******************** Start User Code ***************************
 ;   Add code to display user prompt (including the 'X' / 'O')
 ;   The entered character should be in R1 by the end of this code block.
@@ -486,14 +486,14 @@ GI_LOOP		LD 	R2, GETPLAYER_ADDR
 
 ;   Get User Input
 
-		GETC 
+		GETC
 		PUTC
 		ADD R1, R0, #0
 
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		
-		PUTC 
+
+		PUTC
 
 ; ********************* End User Code ****************************
 ;-----------------------------------------------------------------
@@ -521,15 +521,15 @@ GI_LOOP		LD 	R2, GETPLAYER_ADDR
 
 		LD R4, GI_NEG_ASCII_x
 		ADD R4, R4, R1
-		BRz GI_DONE	
+		BRz GI_DONE
 
-;   Check for less than 1  
+;   Check for less than 1
 
 		LD R4, GI_NEG_ASCII_1
 		ADD R4, R4, R1
 		BRn GI_INVALID
 
-;   Check for greater than 9  
+;   Check for greater than 9
 
 		LD R4, GI_NEG_ASCII_9
 		ADD R4, R4, R1
@@ -589,7 +589,7 @@ GI_R7		.BLKW		1
 ;-----------------------------------------------------------------
 ; DRAWBOARD - Print the board
 ; 1.  Save registers R0 and R7
-; 2.  Print out each row.  Use ROW1_ADDR, ROW2_ADDR, 
+; 2.  Print out each row.  Use ROW1_ADDR, ROW2_ADDR,
 ;     through ROW11_ADDR. Example:
 ;         LD R0, ROW1_ADDR  ; loads ROW1 string pointer into R0
 ; 3.  Restore registers R0 and R7.
@@ -611,77 +611,77 @@ DRAWBOARD
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW2_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW3_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW4_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW5_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW6_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW7_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW8_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW9_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW10_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 		LD R0, ROW11_ADDR
 
 		PUTS
 		AND R0, R0, #0
 		ADD R0, R0, x000A
-		PUTC 
+		PUTC
 
 
 ; Restore R0, R2, R7
@@ -695,7 +695,7 @@ DRAWBOARD
 ; ********************* End User Code ****************************
 ;-----------------------------------------------------------------
 ; PRINTOUTCOME subroutine
-; Args : 
+; Args :
 ;	R1 - Contains a number 1-3
 ;		1 = X wins
 ;		2 = O wins
@@ -740,17 +740,12 @@ O_END
 		AND R0, R0, #0
 		ADD R0, R0, x000A
 		PUTC
-		
+
 ; Restore R4
 		LD R0, GI_R0
 		LD R4, GI_R4
 		LD R7, GI_R7
-
-
-
-
-		
-
+    ;Return
 		RET
 
 OC_CHECK_X	.FILL	x-1
